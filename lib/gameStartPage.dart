4050438adf392d1c589e187b19327fafd1b924cb
ryoya <ryoya47@gmail.com>
2022-04-15 06:47:47 +0900

    mainDevelopVersion1

--- lib/gameStartPage.dart
+++ lib/gameStartPage.dart
@@ -5,16 +5,70 @@ class gameStartPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title : Text("ページ(1)")
-        ),
         body : Center(
-          child: TextButton(
-            child: Text("メインフォームへ戻る"),
-            // （1） 前の画面に戻る
-            onPressed: (){
-              Navigator.pop(context);
-            },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Container(
+                    width: 250,
+                    height: 50,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'add user',
+                        prefixIcon: Icon(
+                          Icons.person,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(32),
+                          borderSide: BorderSide(
+                            color: Colors.grey,
+                            width: 2.0
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add_circle, size: 25,),
+                    color: Colors.blue,
+                    onPressed: () {
+                      // add user
+                    })
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  SizedBox(
+                    width: 150,
+                    child: ElevatedButton(onPressed: (){
+                      Navigator.pop(context);
+                    },
+                      child: Text(
+                          'CANCEL'
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.black12
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 150,
+                    child: ElevatedButton(onPressed: (){
+                      Navigator.pop(context);
+                    },
+                      child: Text(
+                          'START'
+                      ),
+                    ),
+                  ),
+                ],
+              )
+
+            ]
           ),
         )
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ito/mainForm.dart';
 
 import 'gameStartPage.dart';
 
@@ -15,51 +16,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'ito'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
+  // ignore: missing_return
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: ElevatedButton(onPressed: (){
-          Navigator.push(context, MaterialPageRoute(
-              builder: (context) => gameStartPage()
-          ));
-        },
-        child: Text(
-          'START'
-        ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return mainForm();
   }
 }
