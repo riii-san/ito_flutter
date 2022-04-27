a6f6b0c7d58cc597a5b592c30e9c435981e420d8
ryoya <ryoya47@gmail.com>
2022-04-27 20:55:05 +0900

    Openkeyword画面のレイアウト完成

--- lib/openKeyword.dart
+++ lib/openKeyword.dart
@@ -25,47 +25,94 @@ class _openKeywordState extends State<openKeyword> {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
+            SizedBox(height: _config.deviceHeight * 0.1,),
             Row(
               children: <Widget>[
                 SizedBox(width: _config.deviceWidth * 0.03,),
                 for(int i = 0; i < widget._gameMaster.life; i++)
-                  Container(padding: EdgeInsets.all(2), child: Text('❤︎', style: TextStyle(color: Colors.red))),
+                  //Container(padding: EdgeInsets.all(2), child: Text('❤︎', style: TextStyle(color: Colors.red,fontSize: 20))),
+                  Container(
+                    padding: EdgeInsets.all(2),
+                    child: Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    ),
+                  )
               ],
             ),
+            SizedBox(height: _config.deviceHeight * 0.05,),
             Container(
+              alignment: Alignment.center,
               height: _config.deviceHeight * 0.1,
               child: Text(
                 'Question No.' + widget._gameMaster.questionNo.toString(),
                 style: TextStyle(
-                    fontSize: 30,
+                    fontSize: 35,
                     color: Colors.grey
                 ),
               ),
             ),
+            SizedBox(height: _config.deviceHeight * 0.02,),
             Container(
               padding: EdgeInsets.all(20),
-              alignment: Alignment.topLeft,
+              height: _config.deviceHeight * 0.4,
+              width: _config.deviceWidth * 0.9,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              alignment: Alignment.center,
               child: AnimatedTextKit(
                 animatedTexts: [
                   TyperAnimatedText(
-                      'ここに問題が表示されますaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-                      speed: const Duration(milliseconds: 100)
+                    '人気な漫画',
+                    textStyle: TextStyle(
+                      fontSize: 32
+                    ),
+                    speed: const Duration(milliseconds: 100),
                   ),
                 ],
                 isRepeatingAnimation: false,
               ),
             ),
-            SizedBox(
-              width: 150,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('戻る'),
-              ),
-            ),
+            SizedBox(height: _config.deviceHeight * 0.1,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  child: MaterialButton(
+                    height: _config.deviceHeight * 0.1,
+                    minWidth: _config.deviceWidth * 0.1,
+                    onPressed: (){
+                      // TODO : NumberCheck画面へ
+                    },
+                    color: Colors.blue,
+                    shape: CircleBorder(),
+                    child: Icon(
+                      Icons.format_list_numbered,
+                      color: Colors.white,
+                      size: _config.deviceWidth * 0.1,
+                    ),
+                  ),
+                ),
+                SizedBox(width: _config.deviceWidth * 0.2,),
+                SizedBox(
+                  height: _config.deviceHeight * 0.065,
+                  width: _config.deviceWidth * 0.4,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('ANSWER'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red,
+                    ),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
